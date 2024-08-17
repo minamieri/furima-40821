6f9a8dbdec9b4039cd64f28cc5e25dbbe26acc9e
minamieri <sunaga.eri.mail@gmail.com>
2024-08-17 14:57:12 +0900

    モデルのバリデーション、アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,13 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one :shipping
 
-  validates :productname, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :area_id, presence: true
-  validates :days_id, presence: true
-  validates :price, presence: true
-  validates :user_id, presence: true
+  validates :productname, :escription, :category_id, :status_id, :delivery_charge_id, :area_id, :day_id, :price, ;presence: true
 end
