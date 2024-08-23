d8760a3ae2410371ec11f12fe04d82466a7e095b
minamieri <sunaga.eri.mail@gmail.com>
2024-08-23 13:07:17 +0900

    単体テスト

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,18 +3,17 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one :shipping
   has_one_attached :image
-  belongs_to :category
-  belongs_to :status
-  belongs_to :delivery_charge
-  belongs_to :area
-  belongs_to :day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
 
   validates :image, presence: true, unless: :was_attached?
-  validates :productname, :description, :category_id, :status_id, :delivery_charge_id, :area_id, :day_id, :price, presence: true
-  validate :was_attached?
-  # 空の投稿を保存できないようにする
-  validates :description, presence: true
-
+  validates :productname, :description, presence: true
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  # validate :was_attached?
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :status_id, :delivery_charge_id, :area_id, :day_id,
             numericality: { other_than: 1, message: "can't be blank" }
@@ -25,5 +24,6 @@ class Product < ApplicationRecord
     return if image.attached?
 
     errors.add(:image, 'は1枚必要です')
+    # self.image.attached?
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ができる場合' do
+      it '必要な情報がすべて存在すれば保存できること' do
+        expect(@product).to be_valid
+      end
+    end
+
+    context '商品出品ができない場合' do
+      it '商品画像が1枚必須であること' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名が必須であること' do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が必須であること' do
+        @product.description = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリーの入力が必須であること' do
+        @product.category_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '商品の状態の入力が必須であること' do
+        @product.condition_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it '配送料の負担の入力が必須であること' do
+        @product.shipping_fee_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping fee can't be blank")
+      end
+
+      it '発送元の地域の入力が必須であること' do
+        @product.prefecture_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送までの日数の入力が必須であること' do
+        @product.shipping_day_id = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping day can't be blank")
+      end
+
+      it '価格の入力が必須であること' do
+        @product.price = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格が¥300~¥9,999,999の間であること' do
+        @product.price = 299
+        @product.valid?
+        expect(@product.errors.full_messages).to include('Price must be greater than or equal to 300')
+
+        @product.price = 10_000_000
+        @product.valid?
+        expect(@product.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
+      it '価格は半角数値のみ保存可能であること' do
+        @product.price = '１０００'
+        @product.valid?
+        expect(@product.errors.full_messages).to include('Price is not a number')
+      end
+
+      it '販売手数料と販売利益は、小数点以下を切り捨てて表示すること' do
+        @product.price = 1000
+        @product.save
+        expect(@product.fee).to eq((@product.price * 0.1).floor)
+        expect(@product.profit).to eq((@product.price * 0.9).floor)
+      end
+    end
+  end
 end
