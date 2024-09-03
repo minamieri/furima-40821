44ec561263ffc351a58e740ad6bb8bcd17d749f0
minamieri <sunaga.eri.mail@gmail.com>
2024-09-04 01:58:50 +0900

    ログイン中のみ「商品の編集」「削除」ボタンが表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.order('created_at DESC')
@@ -19,10 +20,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
     if current_product.update(product_params)
       redirect_to root_path
     else
@@ -30,10 +37,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:image, :productname, :description, :category_id, :status_id, :delivery_charge_id, :area_id,
                                     :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @product = current_user.products.find_by(id: params[:id])
+    redirect_to products_path, notice: 'Not authorized to edit this product' if @product.nil?
   end
 end
