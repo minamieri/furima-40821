f30bb68dcbef82464b0dbfe4d183097ed35c9e56
minamieri <sunaga.eri.mail@gmail.com>
2024-09-05 21:04:10 +0900

    編集ページの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do 
   devise_for :users #絶対に消しちゃダメ。localhost3000で表示されなくなる。deviseのブラックボックス
   root to: "products#index"
-  resources :products 
+  resources :products
 end
