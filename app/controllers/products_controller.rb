7d438a1e0ada5c59d7207ee1ba4b484b03057773
minamieri <sunaga.eri.mail@gmail.com>
2024-09-07 18:02:48 +0900

    コメントアウト部分の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  # before_action :correct_user, only: [:edit, :update]
   before_action :set_product, only: [:edit, :update]
   before_action :check_user_permission, only: [:edit, :update]
-  # before_action :check_authorization, only: [:show]
 
   def index
     @products = Product.order('created_at DESC')
@@ -16,40 +14,24 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      # redirect_to product_path(@product), notice: '出品しましたぜ.'
       redirect_to root_path, notice: '出品しましたぜ.'
     else
       render :new, status: :unprocessable_entity
-      # render :new
     end
   end
 
   def show
     @product = Product.find(params[:id])
-
-    # if @product.status == '販売中' && @product.user_id != current_user.id
-    # else
-    # redirect_to products_path
-    # end
-
-    # if @product.user_id != current_user.id
-    # else
-    # redirect_to products_path
-    # end
   end
 
   def edit
     @product = Product.find(params[:id])
-    # @product.update(product_params)
-    # redirect_to root_path
   end
 
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product)
-      # if current_product.update(product_params)
-      # redirect_to root_path
     else
       render :edit, status: :unprocessable_entity # エラー記述に関わる部分
     end
@@ -79,14 +61,6 @@ class ProductsController < ApplicationController
       return if @product.user_id == current_user.id
 
       redirect_to root_path
-      # @product = current_user.products.find_by(id: params[:id])
-      # redirect_to products_path, notice: 'Not authorized to edit this product' if @product.nil?
     end
-
-    # def check_authorization
-    # return unless @product.user_id == current_user.id || @product.sold_out?
-
-    # redirect_to products_path
-    # end
   end
 end
