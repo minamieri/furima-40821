d948b52889118f7db64951f5a9b1eb45dafd19f5
minamieri <sunaga.eri.mail@gmail.com>
2024-09-18 11:41:16 +0900

    軽微な修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_product, only: [:index, :create]
   before_action :redirect_if_seller, only: [:index, :create]
+  before_action :move_to_root_path, only: [:index]
   # before_action :set_product, only: [:new, :create]
   def index
     # @order_form = OrderForm.new(order_form_params)　 order_form_params はここで呼び出されていないはず
@@ -46,6 +47,12 @@ class OrdersController < ApplicationController
     # params.require(:order_form).permit(:post_code, :area_id, :city, :address, :building_name, :tel).merge(
     # user_id: current_user.id, product_id: params[:product_id]
     # )
+  end
+
+  def move_to_root_path
+    return unless @product.order_history.present? || @product.user_id == current_user.id
+
+    redirect_to root_path
   end
 
   def pay_item
