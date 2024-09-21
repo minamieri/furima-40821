47272513ab480285774c056e585efa81d347f19e
minamieri <sunaga.eri.mail@gmail.com>
2024-09-21 18:21:34 +0900

    gonの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,6 +16,7 @@ class OrdersController < ApplicationController
       @order_form.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,7 +1,7 @@
 const pay = () => {
   const publicKey = gon.public_key
-  const payjp = Payjp(publicKey)
-  // const payjp = Payjp(PAYJP_PUBLIC_KEY);
+  // const payjp = Payjp(publicKey)
+  const payjp = Payjp(PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -11,6 +11,8 @@ const pay = () => {
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
   const form = document.getElementById('charge-form')
+
+
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
